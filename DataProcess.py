11580b49f42867db797a44f6a3eb5a8ae1a3ebc1
SchulizBee <xj20@illinois.edu>
2024-11-28 19:11:09 -0600

    complete year_range & filter_country for all csv

--- DataProcess.py
+++ DataProcess.py
@@ -118,23 +118,46 @@ def preprocess_csv_type2(file_path, country_column, countries, year_column, year
     return df
 
 
-def preprocess_csv_type3(file_path, country_column, countries, year_range):
+def preprocess_csv_type3(file_path, country_column, countries, year_column, year_range, skip_rows=None):
     """
     To process csv type 3, which doesn't have a column named Date, but every year as 1 column.
     :param file_path: csv file path
     :param country_column: which column to select
     :param countries: list of countries
+    :param year_column: which column to select
     :param year_range: year range
+    :param skip_rows: skip rows until column name occurs
     :return: pd.DataFrame
     """
-    df = pd.read_csv(file_path)
-    # 筛选国家
+    df = pd.read_csv(file_path, skiprows=skip_rows)
     df = filter_by_country(df, country_column, countries)
+    # 自动识别年份列
+    year_columns = [col for col in df.columns if col.isdigit()]  # 判断列名是否为数字
     # 转换宽表为长表
-    year_columns = [col for col in df.columns if col.isdigit()]  # 自动识别年份列
-    df = reshape_dataset(df, id_vars=[country_column], value_vars=year_columns)
-    # 筛选年份
-    df = filter_by_year_range(df, year_range)
+    df = pd.melt(df, id_vars=[country_column], value_vars=year_columns,
+                 var_name=year_column, value_name='Value')  # 'year_column' 是动态的
+
+    df[year_column] = pd.to_numeric(df[year_column], errors='coerce')
+    df = filter_by_year_range(df, year_column, year_range)
+    return df
+
+
+def preprocess_special_csv(file_path, year_column, year_range, skip_rows=None):
+    """
+    Special process for csv which doesn't have a column named Date, and contain only 1 country.
+    :param file_path: csv file path
+    :param year_column: which column to select
+    :param year_range: year range
+    :param skip_rows: skip rows until column name occurs
+    :return: pd.DataFrame
+    """
+    df = pd.read_csv(file_path, skiprows=skip_rows, encoding='latin1')
+
+    df.columns = ['Indicator'] + [str(year) for year in range(2002, 2016)]
+    df = pd.melt(df, id_vars=['Indicator'], var_name=year_column, value_name='Value')
+    df = df[df['Indicator'].str.contains('Unemployment Rate', case=False, na=False)]
+    df[year_column] = pd.to_numeric(df[year_column], errors='coerce')
+    df = filter_by_year_range(df, year_column, year_range)
     return df
 
 
@@ -159,24 +182,163 @@ if __name__ == '__main__':
     )
     #print(CHI_GDP)  # test success
 
-    AUS_OBES = preprocess_csv_type2(
+    AUS_obesity = preprocess_csv_type2(
         file_path='data/Prevalence_of_obesity_among_adults.csv',
         country_column='Location',
         countries=['Australia'],
         year_column='Period',
         year_range=(1995, 2005),
-        skip_rows=0,
+        skip_rows=0
     )
-    #print(AUS_OBES)  # test success
+    #print(AUS_obesity)  # test success
 
-    CHI_OBES = preprocess_csv_type2(
+    CHI_obesity = preprocess_csv_type2(
         file_path='data/Prevalence_of_obesity_among_adults.csv',
         country_column='Location',
         countries=['China'],
         year_column='Period',
         year_range=(2003,2013),
-        skip_rows=0,
+        skip_rows=0
     )
-    #print(AUS_OBES)  # test success
+    #print(CHI_obesity)  # test success
+
+    AUS_underweight = preprocess_csv_type2(
+        file_path='data/Prevalence_of_underweight_among_adults.csv',
+        country_column='Location',
+        countries=['Australia'],
+        year_column='Period',
+        year_range=(1995, 2005),
+        skip_rows=0
+    )
+    #print(AUS_underweight)  # test success
+
+    CHI_underweight = preprocess_csv_type2(
+        file_path='data/Prevalence_of_obesity_among_adults.csv',
+        country_column='Location',
+        countries=['China'],
+        year_column='Period',
+        year_range=(2003, 2013),
+        skip_rows=0
+    )
+    #print(CHI_underweight)  # test success
+
+    AUS_FDI = preprocess_csv_type3(
+        file_path='data/Foreign_Direct _Investment.csv',
+        country_column='Country Name',
+        countries=['Australia'],
+        year_column='Year',
+        year_range=(1995,2005),
+        skip_rows=3
+    )
+    #print(AUS_FDI)  # test success
+
+    CHI_FDI = preprocess_csv_type3(
+        file_path='data/Foreign_Direct _Investment.csv',
+        country_column='Country Name',
+        countries=['China'],
+        year_column='Year',
+        year_range=(2003,2013),
+        skip_rows=3
+    )
+    #print(CHI_FDI)  # test success
+
+    AUS_GHG_emission = preprocess_csv_type3(
+        file_path='data/ghg-emissions.csv',
+        country_column='Country/Region',
+        countries=['Australia'],
+        year_column='Year',
+        year_range=(1995,2005)
+    )
+    #print(AUS_GHG_emission)  # test success
+
+    CHI_GHG_emission = preprocess_csv_type3(
+        file_path='data/ghg-emissions.csv',
+        country_column='Country/Region',
+        countries=['China'],
+        year_column='Year',
+        year_range=(2003,2013)
+    )
+    #print(CHI_GHG_emission)  # test success
+
+    AUS_gov_consume = preprocess_csv_type3(
+        file_path='data/Government_consumption.csv',
+        country_column='Country Name',
+        countries=['Australia'],
+        year_column='Year',
+        year_range=(1995, 2005),
+        skip_rows=3
+    )
+    #print(AUS_gov_consume)  # test success
+
+    CHI_gov_consume = preprocess_csv_type3(
+        file_path='data/Government_consumption.csv',
+        country_column='Country Name',
+        countries=['China'],
+        year_column='Year',
+        year_range=(2003, 2013),
+        skip_rows=3
+    )
+    #print(CHI_gov_consume)  # test success
+
+    AUS_renew_energy = preprocess_csv_type3(
+        file_path='data/Renewable_energy_consumption.csv',
+        country_column='Country Name',
+        countries=['Australia'],
+        year_column='Year',
+        year_range=(1995, 2005),
+        skip_rows=3
+    )
+    #print(AUS_renew_energy)  # test success
+
+    CHI_renew_energy = preprocess_csv_type3(
+        file_path='data/Renewable_energy_consumption.csv',
+        country_column='Country Name',
+        countries=['China'],
+        year_column='Year',
+        year_range=(2003, 2013),
+        skip_rows=3
+    )
+    #print(CHI_renew_energy)  # test success
+
+    AUS_tourism = preprocess_csv_type3(
+        file_path='data/tourism_data.csv',
+        country_column='Country Name',
+        countries=['Australia'],
+        year_column='Year',
+        year_range=(1995, 2005),
+        skip_rows=3
+    )
+    #print(AUS_tourism)  # test success
+
+    CHI_tourism = preprocess_csv_type3(
+        file_path='data/tourism_data.csv',
+        country_column='Country Name',
+        countries=['China'],
+        year_column='Year',
+        year_range=(2003, 2013),
+        skip_rows=3
+    )
+    #print(CHI_tourism)  # test success
+
+    AUS_UR = preprocess_csv_type3(
+        file_path='data/Unemployment_rate_Australia.csv.csv',
+        country_column='Country Name',
+        countries=['Australia'],
+        year_column='Year',
+        year_range=(1995, 2005),
+        skip_rows=4
+    )
+    #print(AUS_UR)  # test success
+
+    CHI_UR = preprocess_special_csv(
+        file_path='data/Unemployment_rate_China.csv',
+        year_column='Year',
+        year_range=(2002, 2013),
+        skip_rows=2
+    )
+    print(CHI_UR)  # test success
+
+
+
 
 
